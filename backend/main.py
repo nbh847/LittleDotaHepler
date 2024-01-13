c3e7d435893d96991bbeb8d62c84690684b802be
bni <759868707@qq.com>
2024-01-13 21:06:22 +0800

    feat:添加英雄站位json数据

--- backend/main.py
+++ backend/main.py
@@ -1,3 +1,6 @@
+import json
+import os
+
 import flask
 import pandas as pd
 
@@ -6,31 +9,56 @@ excel = pd.read_excel("data/arena_data.xlsx")
 
 
 def get_hero():
-    print("获取最新的英雄数据")
+    """
+    获取最新的英雄数据
+    :return:
+    """
+    print("")
     data_map = set()
 
     for index, row in excel.iterrows():
         print(f"第{index + 1}行数据：")
         for inner_index, values in enumerate(row.items()):
             # inner_index: 0-4 是攻, 5-9 是守，10 是进攻胜率
-            print(f"{inner_index}: {values}")
             data_map.add(values[1])
             if inner_index == 9:
                 break
     print(data_map)
+    print(len(data_map))
+
+
+def guess_lineup(line1=""):
+    """
+    猜巅峰竞技场的阵容
+    :return:
+    """
+    pass
+
 
 def run():
     print("小冰冰传奇助手开始运行")
-    data_map = set()
 
-    for index, row in excel.iterrows():
-        print(f"第{index + 1}行数据：")
-        for column, value in row.items():
-            print(f"{column}: {value}")
-            data_map.add(value)
-        break
-    print(data_map)
+    with open("data/heros.json", "r") as file:
+        heros = json.loads(file.read())
+    # 前中后排英雄
+    front_hero = heros[0]
+    middle_hero = heros[1]
+    back_hero = heros[2]
+    print("front_hero:{}".format(front_hero))
+    print("middle_hero:{}".format(middle_hero))
+    print("back_hero:{}".format(back_hero))
+    print("英雄总数:{}".format(len(back_hero) + len(middle_hero) + len(front_hero)))
+
+    # data_map = set()
+    #
+    # for index, row in excel.iterrows():
+    #     print(f"第{index + 1}行数据：")
+    #     for column, value in row.items():
+    #         print(f"{column}: {value}")
+    #         data_map.add(value)
+    #     break
+    # print(data_map)
 
 
 if __name__ == '__main__':
-    get_hero()
+    run()
