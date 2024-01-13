9443b3c5605d1fa0245f3bd7c9f7837620477e0d
bni <759868707@qq.com>
2024-01-13 22:36:33 +0800

    feat:猜巅峰竞技场的阵容

--- backend/main.py
+++ backend/main.py
@@ -5,16 +5,14 @@ import flask
 import pandas as pd
 
 
-excel = pd.read_excel("data/arena_data.xlsx")
-
-
 def get_hero():
     """
     获取最新的英雄数据
     :return:
     """
-    print("")
+    print("获取最新的英雄数据")
     data_map = set()
+    excel = pd.read_excel("data/arena_data.xlsx")
 
     for index, row in excel.iterrows():
         print(f"第{index + 1}行数据：")
@@ -27,12 +25,61 @@ def get_hero():
     print(len(data_map))
 
 
-def guess_lineup(line1=""):
+def get_hero_profiles() -> dict:
     """
-    猜巅峰竞技场的阵容
+    获取所有英雄的各项数据
     :return:
     """
-    pass
+    result = []
+    try:
+        with open("data/hero_profile.json", "r") as file:
+            res = file.read()
+        result = json.loads(res)
+    except Exception as e:
+        print("获取所有英雄的各项数据 error:{}".format(e.args[0]))
+    return result
+
+
+def guess_lineup(my_hero_pool: list, line1: list, line2: list, line3: list):
+    """
+    猜巅峰竞技场的阵容，返回自己英雄池存在的破解阵容
+    :param my_hero_pool: 自己的英雄池
+    :param line1: 第一队
+    :param line2: 第二队
+    :param line3: 第三队
+    :return:
+    """
+    profiles = get_hero_profiles()
+    print(profiles)
+
+
+def generate_latest_attack_lineup() -> list:
+    """
+    根据最新的竞技场对阵数据Excel生成攻击方的阵容(防守方的破解阵容)
+    :return: [["1", "2", "3", "4", "5"], ["23", "24", "25", "26", "27"], ["23", "24", "75", "26", "17"]]
+    """
+    data_map = set()
+    excel = pd.read_excel("data/arena_data.xlsx")
+
+    for index, row in excel.iterrows():
+        print(f"第{index + 1}行数据：")
+        attack_lineup = set()
+        defend_lineup = set()
+        for inner_index, values in enumerate(row.items()):
+            # inner_index: 0-4 是攻, 5-9 是守，10 是进攻胜率
+            print("values", values)
+            if inner_index <= 4:
+                attack_lineup.add(values[1])
+            elif inner_index <= 9:
+                defend_lineup.add(values[1])
+            elif inner_index == 10:
+                winning_rate = float(values[1])
+                if winning_rate >= 0.5:
+                    # 胜率大于 50%
+                    saved_res = {defend_lineup: attack_lineup}
+        break
+    print(data_map)
+    print(len(data_map))
 
 
 def run():
@@ -49,16 +96,8 @@ def run():
     print("back_hero:{}".format(back_hero))
     print("英雄总数:{}".format(len(back_hero) + len(middle_hero) + len(front_hero)))
 
-    # data_map = set()
-    #
-    # for index, row in excel.iterrows():
-    #     print(f"第{index + 1}行数据：")
-    #     for column, value in row.items():
-    #         print(f"{column}: {value}")
-    #         data_map.add(value)
-    #     break
-    # print(data_map)
+    guess_lineup([], [], [], [])
 
 
 if __name__ == '__main__':
-    run()
+    generate_latest_attack_lineup()
