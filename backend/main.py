3219c09775b80e08295776ff4ce6a57b878549d7
bni <759868707@qq.com>
2024-01-15 22:18:30 +0800

    feat:完成普通竞技场的阵容破解模块

--- backend/main.py
+++ backend/main.py
@@ -40,19 +40,6 @@ def get_hero_profiles() -> dict:
     return result
 
 
-def guess_lineup(my_hero_pool: list, line1: list, line2: list, line3: list):
-    """
-    猜巅峰竞技场的阵容，返回自己英雄池存在的破解阵容
-    :param my_hero_pool: 自己的英雄池
-    :param line1: 第一队
-    :param line2: 第二队
-    :param line3: 第三队
-    :return:
-    """
-    profiles = get_hero_profiles()
-    print(profiles)
-
-
 def get_sorted_lineup(lineup) -> str:
     """
     获取排序好的整容
@@ -62,7 +49,7 @@ def get_sorted_lineup(lineup) -> str:
     return ",".join(sorted(list(lineup)))
 
 
-def get_crack_lineup():
+def get_crack_lineup() -> dict:
     """
     获取破解整容的json数据
     :return:
@@ -76,7 +63,50 @@ def get_crack_lineup():
     return result
 
 
-def generate_latest_attack_lineup() -> list:
+def crack_peak_arena_lineup(my_hero_pool: list, lineup1: list, lineup2: list, lineup3: list):
+    """
+    猜 <巅峰竞技场> 的阵容，返回自己英雄池存在的破解阵容
+    :param my_hero_pool: 自己的英雄池
+    :param lineup1: 第一队
+    :param lineup2: 第二队
+    :param lineup3: 第三队
+    :return:
+    """
+    profiles = get_hero_profiles()
+    print(profiles)
+
+
+def crack_arena_lineup(my_hero_pool: list, defend_lineup: list, ignore_hero_pool=False):
+    """
+    根据自己英雄池返回 <普通竞技场> 破解阵容
+    :param my_hero_pool:
+    :param defend_lineup:
+    :return:
+    """
+    all_crack_lineups = get_crack_lineup()
+    sorted_defend_lineup = get_sorted_lineup(defend_lineup)
+    print("对方阵容", sorted_defend_lineup)
+
+    cracked_lineups = all_crack_lineups.get(sorted_defend_lineup)
+    if cracked_lineups:
+        for cracked_lineup in cracked_lineups:
+            lineup = cracked_lineup.get("attack")
+            rate = cracked_lineup.get("rate")
+
+            has_matched_hero = True
+            for hero in lineup.split(","):
+                if hero not in my_hero_pool:
+                    has_matched_hero = False
+                    break
+            if ignore_hero_pool:
+                print("破解阵容", lineup)
+                print("胜率", rate)
+            elif has_matched_hero:
+                print("破解阵容", lineup)
+                print("胜率", rate)
+
+
+def generate_latest_attack_lineup():
     """
     根据最新的竞技场对阵数据Excel生成攻击方的阵容(防守方的破解阵容)
     文件保存到: data/crack_lineup.json
@@ -104,11 +134,18 @@ def generate_latest_attack_lineup() -> list:
                     winning_rate = 0
                 if winning_rate >= 0.5:
                     # 胜率大于 50%
-                    saved_res[get_sorted_lineup(defend_lineup)] = {
-                            "defend": get_sorted_lineup(defend_lineup),
-                            "attack": get_sorted_lineup(attack_lineup),
-                            "rate": "{} %".format(winning_rate * 100)
-                        }
+                    if saved_res.get(get_sorted_lineup(defend_lineup)):
+                        saved_res.get(get_sorted_lineup(defend_lineup)).append({
+                                "defend": get_sorted_lineup(defend_lineup),
+                                "attack": get_sorted_lineup(attack_lineup),
+                                "rate": "{} %".format(winning_rate * 100)
+                            })
+                    else:
+                        saved_res[get_sorted_lineup(defend_lineup)] = [{
+                                "defend": get_sorted_lineup(defend_lineup),
+                                "attack": get_sorted_lineup(attack_lineup),
+                                "rate": "{} %".format(winning_rate * 100)
+                            }]
     print("saved_res", saved_res)
     print("len saved_res", len(saved_res))
     with open("data/crack_lineup.json", "w") as file:
@@ -119,19 +156,9 @@ def generate_latest_attack_lineup() -> list:
 
 def run():
     print("小冰冰传奇助手开始运行")
-
-    with open("data/heros.json", "r") as file:
-        heros = json.loads(file.read())
-    # 前中后排英雄
-    front_hero = heros[0]
-    middle_hero = heros[1]
-    back_hero = heros[2]
-    print("front_hero:{}".format(front_hero))
-    print("middle_hero:{}".format(middle_hero))
-    print("back_hero:{}".format(back_hero))
-    print("英雄总数:{}".format(len(back_hero) + len(middle_hero) + len(front_hero)))
-
-    guess_lineup([], [], [], [])
+    my_heros = ["光法", "大鱼", "巨魔", "影魔", "舞姬", "宙斯", "一姐", "发条", "圣堂", "死灵", "潮汐"]
+    defend_lineup = ["骨王", "火猫", "精灵", "一姐", "圣堂"]
+    crack_arena_lineup(my_heros, defend_lineup, True)
 
 
 if __name__ == '__main__':
@@ -139,4 +166,9 @@ if __name__ == '__main__':
     # get_hero()
     # 生成破解整容
     # generate_latest_attack_lineup()
-    get_crack_lineup()
+    # res = get_crack_lineup()
+    # for key, value in res.items():
+    #     print("key", key)
+    #     print("value", value)
+    # print("len res", len(res))
+    run()
